3125d580e6a86f8da3837fd2e840e91a880beb32
Rahul <rahulpatidar0191@gmail.com>
2022-10-25 13:01:37 -0700

    remove registry part

--- dist/deploy_server.sh
+++ dist/deploy_server.sh
@@ -1,86 +1,40 @@
 #!/bin/bash
 APP_NAME=$1
-SERVER=$2
-SATEL_DOCKER_USER=$3
-SATEL_DOCKER_PASS=$4
-CLIENT_DOCKER_USER=$5
-CLIENT_DOCKER_PASS=$6
-SATEL_REGISTRY=$7
-CLIENT_REGISTRY=$8
-DOCKERFILE=$9
-BRANCH_NAME=${10}
-TAG_NAME=${11}
 
 #echo "TAG_NAME=${TAG_NAME}"
 
-echo "Build Server"
-if [[ $SERVER != None ]]
-then
-    cd $SERVER
-    if [[ $CURRENT_BRANCH_NAME == 'main' ]]
-    then    
-        CLEAN_BRANCH_NAME='main'
-    elif [[ -n $TAG_NAME ]]  
-    then  
-        CLEAN_BRANCH_NAME=$TAG_NAME
-    else
-        CLEAN_BRANCH_NAME=${CURRENT_BRANCH_NAME////_}
-    fi
+echo "Docker up"
+docker-compose -f docker-compose.yml -f docker-compose.pipeline.yml up -d
 
-    cd ..
+echo "DEV image check" 
+IMG_STR=`cat docker-compose.override.yml | grep 'devenv'| cut -d ":" -f 2-3`
+IMG_LIST=( $IMG_STR ) #convert string into an array
+for IMG in "${IMG_LIST[@]}"
+do  
+    echo $IMG
+    DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect ${IMG} > /dev/null || exit 1
+done
 
-    echo "Check if the app uses poetry"
-    if grep -i "poetry" Dockerfile; then
-        EXTRA_ARGUMENTS="--dev"
-    fi
+echo "App health check"
+sleep 5
+docker-compose exec -T webapp python -c "import requests; requests.get('http://localhost:8000/health')" || exit 1
 
-    echo "Build and Push branch image to docker"
-    if [[ -n $TAG_NAME ]]  
-    then
-        DOCKER_USER=$CLIENT_DOCKER_USER
-        DOCKER_PASS=$CLIENT_DOCKER_PASS
-        REGISTRY=$CLIENT_REGISTRY
-    else
-        DOCKER_USER=$SATEL_DOCKER_USER
-        DOCKER_PASS=$SATEL_DOCKER_PASS
-        REGISTRY=$SATEL_REGISTRY
-            
-    fi
-    echo "${DOCKER_USER} ${DOCKER_PASS} ${REGISTRY}"
-    docker login --username=$DOCKER_USER --password=$DOCKER_PASS $REGISTRY
-    docker build -f $DOCKERFILE . -t $REGISTRY/$APP_NAME:$CLEAN_BRANCH_NAME --build-arg DEVFLAG=$EXTRA_ARGUMENTS
-    docker push $REGISTRY/$APP_NAME:$CLEAN_BRANCH_NAME
+echo "Clean up old reports" 
+rm -f unittesting.xml coverage.xml typing.xml
 
-    echo "Docker up"
-    export REGISTRY=$REGISTRY
-    export CLEAN_BRANCH_NAME=$CLEAN_BRANCH_NAME
-    docker-compose -f docker-compose.yml -f docker-compose.pipeline.yml up -d
+echo "Code tests" 
+## Catch the exit codes so we don't exit the whole script before we are done.
+## Typing, linting, formatting check & unit and integration testing
+docker-compose exec -T webapp validatecodeonce; STATUS1=$?
+docker cp "$(docker-compose ps -q webapp)":/python/reports/typing.xml typing.xml
+docker cp "$(docker-compose ps -q webapp)":/python/reports/unittesting.xml unittesting.xml
+docker cp "$(docker-compose ps -q webapp)":/python/reports/coverage.xml coverage.xml
+## Return the status code
+TOTAL=$((STATUS1))
 
-    echo "Clean up old reports" 
-    rm -f unittesting.xml coverage.xml typing.xml
-    
-    # echo "DEV image check" 
-    # IMG_STR=`cat docker-compose.override.yml | grep 'devenv' | cut -d ":" -f 2-3`
-    # DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect ${IMG_STR} > /dev/null || exit 1
-
-
-    echo "App health check"
-    sleep 5
-    docker-compose exec -T webapp python -c "import requests; requests.get('http://localhost:8000/health')" || exit 1
-
-    echo "Code tests" 
-    ## Catch the exit codes so we don't exit the whole script before we are done.
-    ## Typing, linting, formatting check & unit and integration testing
-    docker-compose exec -T webapp validatecodeonce; STATUS1=$?
-    docker cp "$(docker-compose ps -q webapp)":/python/reports/typing.xml typing.xml
-    docker cp "$(docker-compose ps -q webapp)":/python/reports/unittesting.xml unittesting.xml
-    docker cp "$(docker-compose ps -q webapp)":/python/reports/coverage.xml coverage.xml
-    ## Return the status code
-    TOTAL=$((STATUS1))
-    echo "Docker down"
-    docker-compose -f docker-compose.yml -f docker-compose.pipeline.yml down
-    exit $TOTAL
-fi
+echo "Docker down"
+docker-compose -f docker-compose.yml -f docker-compose.pipeline.yml down
+exit $TOTAL
 
 # # if [[ $BRANCH_NAME == 'main' ]]
 # # then
--- dist/index.js
+++ dist/index.js
@@ -4114,18 +4114,8 @@ const exec = __nccwpck_require__(710);
 async function run() {
     try {
         const appName = core.getInput('app-name');
-        const serverPath = core.getInput('server-dir');
-        const satelDockerUser = core.getInput('satel-docker-user');
-        const satelDockerPass = core.getInput('satel-docker-pass');
-        const clientDockerUser = core.getInput('client-docker-user');
-        const clientDockerPass = core.getInput('client-docker-pass');
-        const satelRegistry = core.getInput('satel-registry');
-        const clientRegistry = core.getInput('client-registry');
-        const dockerfile = core.getInput('dockerfile')
-        const currentBranchName = core.getInput('current-branch-name');
-        const tagName = core.getInput('tag-name');
         const src = __dirname;
-        await exec.exec(`${src}/deploy_server.sh  ${appName} ${serverPath} ${satelDockerUser} ${satelDockerPass} ${clientDockerUser} ${clientDockerPass} ${satelRegistry} ${clientRegistry} ${dockerfile} ${currentBranchName} ${tagName}`) ;
+        await exec.exec(`${src}/deploy_server.sh  ${appName}`) ;
     } catch (error) {
         core.setFailed(error.message);
     }
