2c9578f395c4f683595ac529ac46c00ffece550e
Rahul <rahulpatidar0191@gmail.com>
2022-10-26 13:05:04 -0700

    exclude app health check for now

--- dist/deploy_server.sh
+++ dist/deploy_server.sh
@@ -14,13 +14,12 @@ IMG_STR=`cat docker-compose.override.yml | grep 'devenv'| cut -d ":" -f 2-3`
 IMG_LIST=( $IMG_STR ) #convert string into an array
 for IMG in "${IMG_LIST[@]}"
 do  
-    echo $IMG
     DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect ${IMG} > /dev/null || exit 1
 done
 
-echo "App health check"
-sleep 5
-docker-compose exec -T webapp python -c "import requests; requests.get('http://localhost:8000/health')" || exit 1
+# echo "App health check"
+# sleep 5
+# docker-compose exec -T webapp python -c "import requests; requests.get('http://localhost:8000/health')" || exit 1
 
 echo "Clean up old reports" 
 rm -f unittesting.xml coverage.xml typing.xml
