5399254c9d2266fcc62219f5b14186428c7c3643
Rahul <rahulpatidar0191@gmail.com>
2022-10-26 14:07:19 -0700

    remove tests

--- dist/deploy_server.sh
+++ dist/deploy_server.sh
@@ -16,27 +16,15 @@ for IMG in "${IMG_LIST[@]}"
 do  
     DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect ${IMG} > /dev/null || exit 1
 done
-
 # echo "App health check"
 # sleep 5
 # docker-compose exec -T webapp python -c "import requests; requests.get('http://localhost:8000/health')" || exit 1
 
-echo "Clean up old reports" 
-rm -f unittesting.xml coverage.xml typing.xml
 
-echo "Code tests" 
-## Catch the exit codes so we don't exit the whole script before we are done.
-## Typing, linting, formatting check & unit and integration testing
-docker-compose exec -T webapp validatecodeonce; STATUS1=$?
-docker cp "$(docker-compose ps -q webapp)":/python/reports/typing.xml typing.xml
-docker cp "$(docker-compose ps -q webapp)":/python/reports/unittesting.xml unittesting.xml
-docker cp "$(docker-compose ps -q webapp)":/python/reports/coverage.xml coverage.xml
-## Return the status code
-TOTAL=$((STATUS1))
 
-echo "Docker down"
-docker-compose -f docker-compose.yml -f docker-compose.pipeline.yml down
-exit $TOTAL
+# echo "Docker down"
+# docker-compose -f docker-compose.yml -f docker-compose.pipeline.yml down
+
 
 # # if [[ $BRANCH_NAME == 'main' ]]
 # # then
