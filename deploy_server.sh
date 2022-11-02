36d639a28895796ea4457a2161e2d971cbf06932
Rahul <rahulpatidar0191@gmail.com>
2022-11-01 20:07:43 -0700

    add codeowners and update readme

--- deploy_server.sh
+++ deploy_server.sh
@@ -6,9 +6,6 @@ CLEAN_BRANCH_NAME=$3
 export REGISTRY=$REGISTRY
 export CLEAN_BRANCH_NAME=$CLEAN_BRANCH_NAME 
 
-echo "Docker up"
-docker-compose -f docker-compose.yml -f docker-compose.pipeline.yml up -d
-
 echo "DEV image check" 
 IMG_STR=`cat docker-compose.override.yml | grep 'devenv'| cut -d ":" -f 2-3`
 IMG_LIST=( $IMG_STR ) #convert string into an array
@@ -17,6 +14,5 @@ do
     DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect ${IMG} > /dev/null || exit 1
 done
 
-# echo "App health check"
-# sleep 5
-# docker-compose exec -T webapp python -c "import requests; requests.get('http://localhost:8000/health')" || exit 1
+echo "Docker up"
+docker-compose -f docker-compose.yml -f docker-compose.pipeline.yml up -d
